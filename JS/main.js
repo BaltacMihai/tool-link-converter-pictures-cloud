bdf68ccf998c40d1fd53f9bb48dcf941d00a7d44
Baltac <mihai-cristian.baltac@capgemini.com>
2021-08-05 13:31:24 +0300

    Added design

--- JS/main.js
+++ JS/main.js
@@ -16,8 +16,13 @@ function copyToClipboard()
     document.execCommand("copy"); //copy the text from the text field
    // alert("Copied the text: " + url.value);
     test(url.value);
+
+    let popUp = document.getElementById("pop_up");
+    popUp.style.display="block";
+    setTimeout( ()=>popUp.style.display="none", 4000 );
+
 }
 
 function test(url){
-    document.body.style.background = "url('" + url + "')";
+    document.body.style.backgroundImage = "url('" + url + "')";
 }
